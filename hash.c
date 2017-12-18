3a7ee29ec9dbd62c429d54d611f994950d2303c0
Victor MARTINS <vmartins@e1r8p8.42.fr>
2017-12-18 17:04:01 +0100

    finish

--- hash.c
+++ hash.c
@@ -1,84 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hash.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmartins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/18 14:17:05 by vmartins          #+#    #+#             */
+/*   Updated: 2017/12/18 14:23:45 by vmartins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft/libft.h"
 
-typedef struct		s_hash
+typedef struct			s_hash
 {
 	char			*cmd;
 	char			*path;
 	struct s_hash	*next;
 }				       	t_hash;
 
-int hash_table(char *cmd, t_hash *hash)
+t_hash					*ft_new_maillon(char *cmd)
 {
-    int i;
-    int nbrhash;
+	t_hash *new;
 
-    i = 0;
-    nbrhash = 0;
-    while (cmd[i] != '\0')
-    {
-      nbrhash += cmd[i];
-      i++;
-    }
-		nbrhash %= 1000;
-    if (hash[nbrhash].cmd == NULL)
-    {
-			hash[nbrhash].cmd = ft_strdup(cmd);
-			t_hash tmp = hash[nbrhash];
-			tmp.next = NULL;
-
-			ft_putendl(hash[nbrhash].cmd);
-		  hash[nbrhash].next = &tmp;
-    }
-		else
-		{
-			t_hash tmp1 = *hash[nbrhash].next;
-      while (tmp1.next != NULL)
-				tmp1 = *tmp1.next;
-			tmp1.cmd = ft_strdup(cmd);
-			ft_putendl(hash[nbrhash].cmd);
-
-		   hash[nbrhash].next = &tmp1;
-			 ft_putnbr(nbrhash);
-			 ft_putendl(tmp1.cmd);
-    }
-    return(nbrhash);
+	if ((new = (t_hash *)malloc(sizeof(t_hash))) == NULL)
+		return (NULL);
+	new->cmd = ft_strdup(cmd);
+	new->path = NULL;
+	new->next = NULL;
+	return (new);
 }
 
-int main()
+int						hash_table(char *cmd, t_hash *hash)
 {
-  int i;
-  t_hash *hash;
-	hash = malloc(sizeof (t_hash) * 1000);
-	i= 0;
-  ft_memset(hash, 0, sizeof(hash));
+	int		i;
+	int		nbrhash;
+	t_hash	*tmp;
 
-  i = hash_table("abc", hash);
-  i = hash_table("cba", hash);
-	// ft_putnbr(i);
-	ft_putendl(hash[i].cmd);
-	ft_putendl(hash[i].next->cmd);
-	//ft_putnbr(i);
-	//i = 0;
-	//while(i < 630)
-	//{
-		// ft_putendl(hash[0]->cmd);
-		// ft_putendl(hash[0]->cmd);
-		//ft_putnbr(i);
-		//ft_putendl(hash[i].next->cmd);
+	i = 0;
+	nbrhash = 0;
+	while (cmd[i] != '\0')
+	{
+		nbrhash += cmd[i];
+		i++;
+	}
+	nbrhash %= 1000;
+	if (hash[nbrhash].cmd == NULL)
+	{
+		hash[nbrhash].cmd = ft_strdup(cmd);
+		hash[nbrhash].next = NULL;
+	}
+	else if (hash[nbrhash].cmd != NULL)
+	{
+		tmp = &hash[nbrhash];
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = ft_new_maillon(cmd);
+	}
+	return (nbrhash);
+}
 
-		// if (hash[i].next != NULL)
-		// 	{
-		// 		ft_putendl("LOLOLO");
-    //
-		// 	//	t_hash *tmp = hash[i].next;
-		// 		while (hash[i].next != NULL) {
-		// 			ft_putendl("++++");
-		// 			ft_putendl(hash[i].next->cmd);
-		// 			ft_putendl("++++");
-		// 			hash[i].next = hash[i].next->next;
-		// 		}
-		// 	}
-		// i++;
-//	}
-  return (0);
+int						main(void)
+{
+	int		i;
+	int		j;
+	t_hash	*hash;
+
+	i = 0;
+	j = 0;
+	hash = malloc(sizeof(t_hash) * 1000);
+	ft_memset(hash, 0, sizeof(hash));
+	i = hash_table("cba", hash);
+	i = hash_table("abc", hash);
+	i = hash_table("cba", hash);
+	i = hash_table("abc", hash);
+	i = hash_table("bca", hash);
+	i = hash_table("acb", hash);
+	i = hash_table("toto", hash);
+	while (j < 1000)
+	{
+		if (hash[j].cmd != NULL)
+		{
+			ft_putendl("NEW");
+			ft_putendl(hash[j].cmd);
+			if (hash[j].next != NULL)
+			{
+				ft_putendl("COLLISSION");
+				while (hash[j].next)
+				{
+					hash[j] = *hash[j].next;
+					ft_putendl(hash[j].cmd);
+				}
+			}
+		}
+		j++;
+	}
+	return (0);
 }
